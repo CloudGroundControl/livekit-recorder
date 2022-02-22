1db37244d6d8827a1482d3f22fa024f0f4a3ba39
Dennis Wirya <dennis.wirya@advancednavigation.com>
2022-02-22 15:23:11 +1100

    chore: Add integration test with LK for recording use-case
    Code coverage: 93.1% in `recorder` & 100% in `internal/static`

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/labstack/echo/v4 v4.6.3
 	github.com/livekit/protocol v0.11.12
+	github.com/livekit/server-sdk-go v0.8.5
 	github.com/pion/rtp v1.7.4
 	github.com/pion/transport v0.13.0
 	github.com/pion/webrtc/v3 v3.1.23
@@ -12,13 +13,26 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bep/debounce v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/eapache/channels v1.1.0 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/go-logr/logr v1.1.0 // indirect
+	github.com/go-logr/stdr v1.0.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.3 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jxskiss/base62 v0.0.0-20191017122030-4f11678b909b // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pion/datachannel v1.5.2 // indirect
 	github.com/pion/dtls/v2 v2.1.2 // indirect
 	github.com/pion/ice/v2 v2.1.20 // indirect
@@ -34,6 +48,12 @@ require (
 	github.com/pion/turn/v2 v2.0.6 // indirect
 	github.com/pion/udp v0.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/thoas/go-funk v0.9.0 // indirect
+	github.com/twitchtv/twirp v8.1.0+incompatible // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
@@ -42,5 +62,9 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pkg/recorder/recorder_test.go
+++ pkg/recorder/recorder_test.go
@@ -2,10 +2,17 @@ package recorder
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/cloudgroundcontrol/livekit-egress/pkg/recorder/internal/static"
+	"github.com/livekit/protocol/auth"
+	lksdk "github.com/livekit/server-sdk-go"
 	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
 	"github.com/stretchr/testify/require"
 )
 
@@ -83,6 +90,7 @@ func TestWritePacketsWithSampleBuffer(t *testing.T) {
 	sink := NewBufferSink("test")
 	tr, _ := NewTrackRecorder(codec, sink)
 	rec := promoteRecorder(tr)
+	require.NotNil(t, rec.sb)
 
 	// Write multiple packets
 	for i := 0; i < 10; i++ {
@@ -105,6 +113,7 @@ func TestWritePacketsWithoutSampleBuffer(t *testing.T) {
 	sink := NewBufferSink("test")
 	tr, _ := NewTrackRecorder(codec, sink)
 	rec := promoteRecorder(tr)
+	require.Nil(t, rec.sb)
 
 	// Write multiple packets
 	for i := 0; i < 10; i++ {
@@ -138,4 +147,123 @@ func TestStopRecordingWithoutStart(t *testing.T) {
 		_, ok = (<-rec.closed)
 		require.True(t, ok)
 	}()
+}
+
+func getEnvOrFail(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		log.Fatalf("%s not set", key)
+	}
+	return val
+}
+
+func TestRecorderUsageScenario(t *testing.T) {
+	url := getEnvOrFail("LIVEKIT_URL")
+	apiKey := getEnvOrFail("LIVEKIT_API_KEY")
+	apiSecret := getEnvOrFail("LIVEKIT_API_SECRET")
+	testRoomName := "livekit-egress-test"
+	TRUE := true
+	FALSE := false
+	participantID := "lk-participant"
+	recorderID := "lk-recorder"
+
+	// -----
+	// Generate access tokens for participant and recorder
+	// -----
+
+	pAT := auth.NewAccessToken(apiKey, apiSecret)
+	pGrant := &auth.VideoGrant{
+		RoomJoin:     true,
+		Room:         testRoomName,
+		CanPublish:   &TRUE,
+		CanSubscribe: &TRUE,
+	}
+	pAT.AddGrant(pGrant).SetIdentity(participantID).SetValidFor(time.Hour)
+	pToken, err := pAT.ToJWT()
+	require.NoError(t, err)
+
+	rAT := auth.NewAccessToken(apiKey, apiSecret)
+	rGrant := &auth.VideoGrant{
+		RoomJoin:       true,
+		Room:           testRoomName,
+		CanPublish:     &FALSE,
+		CanPublishData: &FALSE,
+		CanSubscribe:   &TRUE,
+		Hidden:         true,
+		Recorder:       true,
+	}
+	rAT.AddGrant(rGrant).SetIdentity(recorderID).SetValidFor(time.Hour)
+	rToken, err := rAT.ToJWT()
+	require.NoError(t, err)
+
+	// -----
+	// Connect to room
+	// -----
+
+	var pRoom, rRoom *lksdk.Room
+
+	pRoom, err = lksdk.ConnectToRoomWithToken(url, pToken)
+	require.NoError(t, err)
+
+	rRoom, err = lksdk.ConnectToRoomWithToken(url, rToken)
+	require.NoError(t, err)
+
+	// -----
+	// Create track for participant to publish
+	// -----
+
+	preferredCodec := webrtc.MimeTypeVP8
+
+	sampleTrack, err := lksdk.NewLocalSampleTrack(webrtc.RTPCodecCapability{
+		MimeType:  preferredCodec,
+		ClockRate: 90000,
+		Channels:  1,
+	})
+	require.NoError(t, err)
+
+	pRoom.LocalParticipant.PublishTrack(sampleTrack, participantID+"-video")
+
+	// -----
+	// Publish static media packets until we receive stop signal
+	// -----
+
+	sampleProvider := static.NewProvider()
+	sampleDone := make(chan struct{}, 1)
+	go func() {
+		var sample media.Sample
+		for {
+			select {
+			case <-sampleDone:
+				return
+			default:
+				sample, err = sampleProvider.NextSample()
+				require.NoError(t, err)
+				sampleTrack.WriteSample(sample, nil)
+			}
+		}
+	}()
+
+	// -----
+	// Create recorder since we know the codec we'll be publishing
+	// -----
+
+	sink := NewBufferSink(participantID + "-video")
+	rec, err := NewTrackRecorder(webrtc.RTPCodecParameters{
+		RTPCodecCapability: webrtc.RTPCodecCapability{
+			MimeType: webrtc.MimeTypeVP8,
+		},
+	}, sink)
+	require.NoError(t, err)
+
+	rRoom.Callback.OnTrackSubscribed = func(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
+		rec.Start(track)
+	}
+
+	// -----
+	// Let participant publish track for a while,
+	// then stop publishing and recording
+	// -----
+	time.Sleep(time.Second * 2)
+	sampleDone <- struct{}{}
+	rec.Stop()
 }
